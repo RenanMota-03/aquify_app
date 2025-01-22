9af801cb1a17d75d0bc5ef884edc3c5a6f7853ea
Renan <renan.mota2534@gmail.com>
2025-01-22 15:59:52 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/aquify_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.aquify_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/common/constants/app_colors.dart
@@ -0,0 +1,25 @@
+import 'dart:ui';
+
+class AppColors {
+  AppColors._();
+  static const Color greenOne = Color(0xFF63B5AF);
+  static const Color greenTwo = Color(0xFF438883);
+  static const List<Color> blueGradient = [
+    Color(0xFFE0F7FA),
+    Color(0xFFB3E5FC),
+    Color(0xFF81D4FA),
+  ];
+  static const List<Color> blue2Gradient = [
+    Color(0xFFB3E5FC),
+    Color(0xFF81D4FA),
+  ];
+  static const List<Color> greyGradient = [
+    Color(0xFFB5B5B5),
+    Color(0xFF7F7F7F),
+  ];
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color iceWhite = Color(0xFFEEF8F7);
+  static const Color darkGrey = Color(0xFF444444);
+  static const Color grey = Color(0xFF666666);
+  static const Color error = Color(0xFFF44336);
+}
